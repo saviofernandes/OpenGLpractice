906692050bf3f960675be739e68ca2317dc77ebb
saviofernandes <savio_fernandes_89@outlook.com>
2019-10-05 22:10:40 +0400

    multiple occurences of the same cube

--- opengl/Source.cpp
+++ opengl/Source.cpp
@@ -18,6 +18,8 @@ float x = 0.0f;		//location of triangle on x-axis
 float inc = 0.01f;	//offset for moving the triangle
 float cameraX, cameraY, cameraZ; 
 float cubeLocX, cubeLocY, cubeLocZ;
+int i;
+float tf; //Time factor
 
 // allocate variables used in display() function, so that they won’t need to be allocated during rendering 
 GLuint mvLoc, projLoc; 
@@ -61,7 +63,7 @@ void init(GLFWwindow* window)
 	//Camera setup
 	cameraX = 0.0f; 
 	cameraY = 0.0f; 
-	cameraZ = 8.0f;
+	cameraZ = 20.0f;
 
 	cubeLocX = 0.0f;
 	cubeLocY = -2.0f; // shift down Y to reveal perspective
@@ -89,34 +91,41 @@ void display(GLFWwindow* window, double currentTime) {
 	aspect = float(width) / float(height);
 	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
 	
-	//Build the view matrix, model matrix and model-view matrix
-	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-	//mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
 	
-	//Use current time to compute different translations in x, y, and z
-	tMat = glm::translate(glm::mat4(1.0f), glm::vec3(sin(0.35f * currentTime) * 2.0f, cos(0.52f * currentTime) * 2.0f, sin(0.7f * currentTime) * 2.0f));
-	rMat = glm::rotate(glm::mat4(1.0f), 1.75f * (float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
-	rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(1.0f, 0.0f, 0.0f));
-	rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
+															 
+	//Each iteration is for an individual cube
+	for (i = 0; i < 24; i++)
+	{
+		tf = currentTime + i;
 
-	mMat = tMat * rMat;
+		//Build the view matrix, model matrix and model-view matrix
+		vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+		//mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
 
-	mvMat = vMat * mMat;
+		//Use current time to compute different translations in x, y, and z
+		tMat = glm::translate(glm::mat4(1.0f), glm::vec3(sin(0.35f * tf) * 8.0f, cos(0.52f * tf) * 8.0f, sin(0.7f * tf) * 8.0f));
+		rMat = glm::rotate(glm::mat4(1.0f), 1.75f * tf, glm::vec3(0.0f, 1.0f, 0.0f));
+		rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(1.0f, 0.0f, 0.0f));
+		rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(0.0f, 0.0f, 1.0f));
 
-	//Copy perspective and MV matrices to corresponding uniform variables
-	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+		mMat = tMat * rMat;
 
-	//Associate VBO with the corresponding vertex attribute in the vertex shader
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
+		mvMat = vMat * mMat;
 
-	//Adjust OpenGL settings and draw the model
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+		//Copy perspective and MV matrices to corresponding uniform variables
+		glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
+		//Associate VBO with the corresponding vertex attribute in the vertex shader
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+		glEnableVertexAttribArray(0);
+
+		//Adjust OpenGL settings and draw the model
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LEQUAL);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
 }
 int main(void) {
 	if (!glfwInit()) { exit(EXIT_FAILURE); }
